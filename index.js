2ad22998f138ba309b5bfee523c4c52b9b0fb0b5
Cayden <caydenfox12@outlook.com>
2021-08-04 22:01:04 -0600

    More HTML and CSS layout, beginning JS

---
+++ index.js
@@ -0,0 +1,35 @@
+let text = "heyo string"
+    console.log(text)
+    console.log('hello')
+    console.log('show me the text')
+    
+
+
+let newPlant = document.querySelector("#newPlant")
+newPlant.addEventListener('click', (event) => newPlantTile())
+
+function newPlantTile() {
+    let container = document.querySelector('#plantsbox');
+    let div = document.createElement('div');
+    div.innerHTML = `
+  
+                    <img src="./lauren-mancke-DpphPG9ENsI-unsplash.jpg" width="2600" height="4000">
+                    <h1 class="name">Fiddle-Leaf Fig</h1>
+                    <p class="water">Water once every: <select>
+                        <option>Day</option>
+                        <option>Week</option>
+                        <option>1-2 Weeks</option>
+                        <option>2-3 Weeks</option>
+                        <option>Month</option>
+                    </select></p>
+                    <p class="light">This plant likes: <select>
+                        <option>Direct light</option>
+                        <option>Bright, indirect light</option>
+                        <option>Medium light</option>
+                        <option>Low light</option>
+                    </select></p>
+                    <p class="other">Notes: <textarea></textarea></p>`
+        div.classList.add("plantcontainer")
+    container.append(div);
+    console.log("this function is running")
+}
