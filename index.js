5ac761a7296a14a1922d0b6b9fe2b909e4bfe2e8
Cayden <caydenfox12@outlook.com>
2021-08-08 21:39:47 -0600

    included javascript to bring up new plant window when clicking the add button

--- index.js
+++ index.js
@@ -6,7 +6,48 @@ let text = "heyo string"
 
 
 let newPlant = document.querySelector("#newPlant")
-newPlant.addEventListener('click', (event) => newPlantTile())
+newPlant.addEventListener('click', (event) => newPlantWindow())
+
+
+function newPlantWindow() {
+    let body = document.querySelector('#body')
+    let container = document.createElement('div');
+    container.id = 'newPlantWindow';
+    let div = document.createElement('div');
+    div.id = 'window';
+    div.innerHTML = `
+                    <div class="plantcontainer">
+                    <img src="./lauren-mancke-DpphPG9ENsI-unsplash.jpg" width="2600" height="4000">
+                    <h1 class="name">Fiddle-Leaf Fig</h1>
+                    <p class="water">Water once every: <select>
+                        <option>Day</option>
+                        <option>Week</option>
+                        <option>1-2 Weeks</option>
+                        <option>2-3 Weeks</option>
+                        <option>Month</option>
+                    </select></p>
+                    <p class="light">This plant likes: <select>
+                        <option>Direct light</option>
+                        <option>Bright, indirect light</option>
+                        <option>Medium light</option>
+                        <option>Low light</option>
+                    </select></p>
+                    <p class="other">Notes: <textarea></textarea></p>
+                </div>
+                <button id="addNew">Add</button>
+                    `
+    container.append(div);
+    body.append(container);
+    console.log("this function is running")
+}
+
+function closeWindow() {
+    let window = document.querySelector('#newPlantWindow');
+    window.innerHTML = ``;
+    window.id = '';
+}
+
+
 
 function newPlantTile() {
     let container = document.querySelector('#plantsbox');
@@ -33,3 +74,6 @@ function newPlantTile() {
     container.append(div);
     console.log("this function is running")
 }
+
+let windowbg = document.querySelector('#newPlantWindow')
+windowbg.addEventListener('click', (event) => closeWindow())
