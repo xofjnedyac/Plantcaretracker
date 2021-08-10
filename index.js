81454099356d0757e64bd86c997375a9774c2dbc
Cayden <caydenfox12@outlook.com>
2021-08-09 22:53:32 -0600

    add new plant to list when clicking the add button in the new plant window

--- index.js
+++ index.js
@@ -16,29 +16,43 @@ function newPlantWindow() {
     let div = document.createElement('div');
     div.id = 'window';
     div.innerHTML = `
-                    <div class="plantcontainer">
-                    <img src="./lauren-mancke-DpphPG9ENsI-unsplash.jpg" width="2600" height="4000">
-                    <h1 class="name">Fiddle-Leaf Fig</h1>
-                    <p class="water">Water once every: <select>
-                        <option>Day</option>
-                        <option>Week</option>
-                        <option>1-2 Weeks</option>
-                        <option>2-3 Weeks</option>
-                        <option>Month</option>
-                    </select></p>
-                    <p class="light">This plant likes: <select>
-                        <option>Direct light</option>
-                        <option>Bright, indirect light</option>
-                        <option>Medium light</option>
-                        <option>Low light</option>
-                    </select></p>
-                    <p class="other">Notes: <textarea></textarea></p>
-                </div>
-                <button id="addNew">Add</button>
+                <div class="plantcontainer">
+                <!-- <img src="./lauren-mancke-DpphPG9ENsI-unsplash.jpg" width="2600" height="4000"> -->
+                <i class="fas fa-leaf fa-10x"></i>
+                <input type="text" value="My Plant" id="plantName">
+                <p class="water">Water once every: <select>
+                    <option>Day</option>
+                    <option>Week</option>
+                    <option>1-2 Weeks</option>
+                    <option>2-3 Weeks</option>
+                    <option>Month</option>
+                </select></p>
+                <p class="light">This plant likes: <select>
+                    <option>Direct light</option>
+                    <option>Bright, indirect light</option>
+                    <option>Medium light</option>
+                    <option>Low light</option>
+                </select></p>
+                <p class="other">Notes: <textarea></textarea></p>
+            </div>
+            <button id="addNew">Add</button>
                     `
     container.append(div);
     body.append(container);
     console.log("this function is running")
+
+    let addNew = document.querySelector("#addNew");
+    function returnToHome() {
+        container.remove();
+    };
+
+    let plantNameInput = document.querySelector('#plantName')
+    let plantName = plantNameInput.Value;
+    console.log(plantName);
+
+    addNew.addEventListener('click', (event) => newPlantTile());
+    addNew.addEventListener('click', (event) => returnToHome());
+    
 }
 
 function closeWindow() {
@@ -76,4 +90,4 @@ function newPlantTile() {
 }
 
 let windowbg = document.querySelector('#newPlantWindow')
-windowbg.addEventListener('click', (event) => closeWindow())
+// windowbg.addEventListener('click', (event) => closeWindow())
